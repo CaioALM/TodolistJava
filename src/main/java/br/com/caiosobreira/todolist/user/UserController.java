25876451f5bec25a322c721cefdb15d395bc7c58
Caio <caiosobreira77@gmail.com>
2023-10-16 19:21:51 -0300

    chore: add creation user to database

--- src/main/java/br/com/caiosobreira/todolist/user/UserController.java
+++ src/main/java/br/com/caiosobreira/todolist/user/UserController.java
@@ -2,6 +2,7 @@ package br.com.caiosobreira.todolist.user;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -9,8 +10,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/")
-   public void create(@RequestBody UserModel userModel) {
-    System.out.println(userModel.getName());
+   public UserModel create(@RequestBody UserModel userModel) {
+    var userCreated = this.userRepository.save(userModel);
+    return userCreated; 
    } 
 }
